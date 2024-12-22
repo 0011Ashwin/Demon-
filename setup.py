00a28fa71f34821b2f2e7b01619b216fa88094ba
Ashwin <ashwinmehta1234500@gmail.com>
2024-12-22 13:29:04 +0530

    setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
-## mapping the whole packages for appliction
+## Import necessary Libraries
 from setuptools import find_packages, setup
 from typing import List
 
+## Constants
 HYPEN_E_DOT = '-e .'
 
 def get_requirements(file_path:str)->List[str]:
@@ -10,22 +11,24 @@ def get_requirements(file_path:str)->List[str]:
     """
     requirements=[]
     
+    # Read the requirements file
     with open(file_path) as file_obj:
         requirements = file_obj.readlines()
+        # Remove the new line from character for requriements
         requirements=[req.replace('\n', "") for req in requirements]
-        
+        # Remove the empty string from the list 
         if HYPEN_E_DOT in requirements:
             requirements.remove(HYPEN_E_DOT)
             
     return requirements
 
 
-
+## Setup the packages and packages details
 setup(
-name="mlproject",
-version="0.0.1",
-author="Ashwin",
-author_email="ashwinmehta1234500@gmail.com",
-packages=find_packages(),  
-install_requires = get_requirements("requirements.txt")
+name="mlproject",  # Name of the package 
+version="0.0.1",   # Version of the package
+author="Ashwin",   # Author of the package
+author_email="ashwinmehta1234500@gmail.com",  # Author email
+packages=find_packages(),   # automatically find the packages for the project
+install_requires = get_requirements("requirements.txt") # Get the requriements for the file dependencies.
 )
